f3fcbc8cb61144b765a26b2c57bc72490ce36cb6
luc_pimentel <lucas_cpimentel@proton.me>
2025-02-21 00:01:07 -0300

    add debug line

--- src/Server.cs
+++ src/Server.cs
@@ -16,6 +16,7 @@ public class Server
         _directory = args.Length > 1 && args[0] == "--directory" ? args[1] : "";
         server = new TcpListener(IPAddress.Any, 4221);  // Initialize here
         server.Start();
+        Console.WriteLine("Server started on port 4221");
         
         while (true)
         {
