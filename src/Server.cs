0d40957edf12953a7a101cecc032ea5e1f09e28b
luc_pimentel <lucas_cpimentel@proton.me>
2025-02-20 23:14:46 -0300

    200 response

--- src/Server.cs
+++ src/Server.cs
@@ -4,7 +4,20 @@ using System.Net.Sockets;
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 Console.WriteLine("Logs from your program will appear here!");
 
-// Uncomment this block to pass the first stage
+// ... existing code ...
+
 TcpListener server = new TcpListener(IPAddress.Any, 4221);
 server.Start();
-server.AcceptSocket(); // wait for client
+
+while (true)
+{
+    TcpClient client = server.AcceptTcpClient();
+    NetworkStream stream = client.GetStream();
+
+    // Send HTTP response
+    string response = "HTTP/1.1 200 OK\r\n\r\n";
+    byte[] responseBytes = System.Text.Encoding.ASCII.GetBytes(response);
+    stream.Write(responseBytes, 0, responseBytes.Length);
+
+    client.Close();
+}
