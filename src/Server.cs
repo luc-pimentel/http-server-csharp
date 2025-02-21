701c8b7c04f9084d31d61db94424e533eeb698d9
luc_pimentel <lucas_cpimentel@proton.me>
2025-02-20 23:48:24 -0300

    Add files endpoint II

--- src/Server.cs
+++ src/Server.cs
@@ -1,6 +1,6 @@
 using System.Net;
 using System.Net.Sockets;
-
+using System.IO;
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 Console.WriteLine("Logs from your program will appear here!");
 // ... existing code ...
