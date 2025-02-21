363c31a6f2b4470f2e82be412b4d4879a2b37251
luc_pimentel <lucas_cpimentel@proton.me>
2025-02-20 23:25:36 -0300

    Add status 4040

--- src/Server.cs
+++ src/Server.cs
@@ -4,6 +4,7 @@ using System.Net.Sockets;
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 Console.WriteLine("Logs from your program will appear here!");
 
+
 // ... existing code ...
 
 TcpListener server = new TcpListener(IPAddress.Any, 4221);
@@ -14,8 +15,28 @@ while (true)
     TcpClient client = server.AcceptTcpClient();
     NetworkStream stream = client.GetStream();
 
-    // Send HTTP response
-    string response = "HTTP/1.1 200 OK\r\n\r\n";
+    // Read the HTTP request
+    byte[] buffer = new byte[1024];
+    int bytesRead = stream.Read(buffer, 0, buffer.Length);
+    string request = System.Text.Encoding.ASCII.GetString(buffer, 0, bytesRead);
+    Console.WriteLine("request: " + request);
+
+    // Parse the path from the request
+    string[] requestLines = request.Split('\n');
+    string[] requestParts = requestLines[0].Split(' ');
+    string path = requestParts[1];
+
+    // Prepare response based on path
+    string response;
+    if (path == "/")
+    {
+        response = "HTTP/1.1 200 OK\r\n\r\n";
+    }
+    else
+    {
+        response = "HTTP/1.1 404 Not Found\r\n\r\n";
+    }
+
     byte[] responseBytes = System.Text.Encoding.ASCII.GetBytes(response);
     stream.Write(responseBytes, 0, responseBytes.Length);
 
